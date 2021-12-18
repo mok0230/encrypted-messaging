4a80db769b10d43b2ddec01cf7b5b9ca279267d7
mok <mok0230@protonmail.com>
2021-12-18 09:39:42 -0600

    change deploy script to deployProxy

---
+++ scripts/deployProxy.js
@@ -0,0 +1,23 @@
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const weiAmount = (await deployer.getBalance()).toString();
+  
+  console.log("Account balance:", (await ethers.utils.formatEther(weiAmount)));
+
+  const Contract = await ethers.getContractFactory("OnChainMessengerV1");
+  const contract = await upgrades.deployProxy(Contract);
+
+  console.log("Contract address:", contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
